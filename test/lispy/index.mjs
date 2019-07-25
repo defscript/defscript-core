3a44dfd50fb4506abbe1eaa94c629009c27653ac
Gabriel Ratener <gabe9693@gmail.com>
2019-07-25 17:58:37 -0500

    Improve embedded language API

---
+++ test/lispy/index.mjs
@@ -0,0 +1,22 @@
+
+import {EmbeddedLanguage} from "../../lib/embedded"
+import {Parser} from "./lispy-parser.out"
+import Lexer from "lexie"
+
+const lexer = new Lexer([
+    {
+
+    }
+]);
+
+export class EmbeddedLispy extends EmbeddedLanguage {
+    compile() {
+
+
+        this.context
+    }
+
+    transform(node) {
+
+    }
+}
